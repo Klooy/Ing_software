cfbd5d18a4113f32888678c9e4027ca81e88e50c
earleysanabria <arleysanabria.1523@gmail.com>
2021-12-12 11:08:44 -0500

    Funcion GestionUsuarios Subido por Edson Sanabria

---
+++ Edson Arley Sanabria Valencia/Gestion de usuarios/HappyShop/src/app/info/paypolicy/paypolicy.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-paypolicy',
+  templateUrl: './paypolicy.component.html',
+  styleUrls: ['./paypolicy.component.css']
+})
+export class PaypolicyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
